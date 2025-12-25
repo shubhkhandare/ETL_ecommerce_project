1fe2f19565256835e68c4d64d8426b3458bcdc4f
shubhkhandare <khandareshubh5@gmail.com>
2025-12-25 17:17:22 +0000

    dim_bronze complete commit

--- 01_medallion_processing_dim/1_dim_bronze.py
+++ 01_medallion_processing_dim/1_dim_bronze.py
@@ -164,3 +164,104 @@ df.write\
 # COMMAND ----------
 
 df.show(3)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## order_items
+# MAGIC
+
+# COMMAND ----------
+
+path = "/Volumes/ecommerce/raw/source_date/order_items/landing/*.csv"
+
+# df = spark.read.option("header", True).csv(path)
+# df.limit(3).display()
+
+# COMMAND ----------
+
+order_items_schema = StructType([
+    StructField('order_id', StringType(),True),
+    StructField('item_seq', StringType(),True),
+    StructField('product_id', StringType(),True),
+    StructField('quantity', StringType(),True),
+    StructField('unit_price_currency', StringType(),True),
+    StructField('unit_price', StringType(),True),
+    StructField('discount_pct', StringType(),True),
+    StructField('tax_amount', StringType(),True),
+    StructField('channel', StringType(),True),
+    StructField('coupon_code', StringType(),True),
+
+])
+
+
+# COMMAND ----------
+
+df = spark.read.option("header", True).option("delimiter", ",").schema(order_items_schema).csv(path)\
+
+df = df.withColumn("_source_file",
+    col("_metadata.file_path")
+).withColumn(
+    "ingestion_time",
+    current_timestamp())
+
+df.limit(3).display()
+
+
+# COMMAND ----------
+
+df.write\
+    .mode("overwrite")\
+    .format("delta")\
+    .option("overwriteSchema", "true")\
+    .saveAsTable(f"{catalog_name}.bronze.bronze_order_items")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##products
+
+# COMMAND ----------
+
+raw_product_path = "/Volumes/ecommerce/raw/source_date/products/*.csv"
+
+df = spark.read.option("header", True).csv(raw_product_path)
+df.limit(3).display()
+
+
+# COMMAND ----------
+
+
+product_schema = StructType([
+    StructField('product_id', StringType(),True),
+    StructField('sku', StringType(),True),
+    StructField('category_code', StringType(),True),
+    StructField('brand_code', StringType(),True),
+    StructField('color', StringType(),True),
+    StructField('size', StringType(),True),
+    StructField('material', StringType(),True),
+    StructField('weight_grams', StringType(),True),
+    StructField('length_cm', StringType(),True),
+    StructField('width_cm', StringType(),True),
+    StructField('height_cm', StringType(),True),
+    StructField('rating_count', StringType(),True),
+])
+
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col, current_timestamp
+df = spark.read.option("header", True).option("delimiter", ",").schema(product_schema).csv(raw_product_path)
+
+df = df.withColumn("_source_file", col("_metadata.file_path"))\
+    .withColumn("ingestion_time", current_timestamp())
+
+df.limit(4).display()
+
+# COMMAND ----------
+
+df.write\
+    .mode("overwrite")\
+    .format("delta")\
+    .option("overwriteSchema", "true")\
+    .saveAsTable(f"{catalog_name}.bronze.bronze_products")
